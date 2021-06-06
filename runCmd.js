6a5617aa8af1de4187bc48e020e8a15bd369c4e2
Eric Vallee <eric_vallee@webificservices.com>
2021-06-06 14:03:59 -0400

    fix: #370 Increased commands max buffer size to 100mb

--- runCmd.js
+++ runCmd.js
@@ -2,11 +2,13 @@ const childProcess = require('child_process')
 
 const core = require('@actions/core')
 
-function runCmd (cmd, label = '') {
+const DEFAULT_MAX_BUFFER = 100 * 1024 * 1024
+
+function runCmd (cmd, label = '', maxBuffer = DEFAULT_MAX_BUFFER) {
   if (label.length > 0) {
     core.info(label)
   }
-  const result = childProcess.spawnSync(cmd[0], cmd.slice(1))
+  const result = childProcess.spawnSync(cmd[0], cmd.slice(1), {"maxBuffer": maxBuffer})
   if (result.stdout.length > 0) {
     core.info(result.stdout.toString())
   }
